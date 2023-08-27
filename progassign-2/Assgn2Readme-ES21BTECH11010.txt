0e46633551e94dc09bd1084047a4a6cd97b627dc
Bapatu Manoj Kumar Reddy <109718754+ManojKumar-02@users.noreply.github.com>
2023-08-27 22:16:44 +0530

    Add files via upload

---
+++ progassign-2/Assgn2Readme-ES21BTECH11010.txt
@@ -0,0 +1,26 @@
+#The following are the instructions for compiling and executing the .cpp files 
+    
+For Pthread :
+  compiling : g++-12 -o pthread.out Assgn2Srcpthread-CS21BTECH11043.cpp -lpthread
+  execution : ./pthread.out
+	
+For OpenMP  :
+  compiling : g++-12 -o openmp.out Assgn2SrcOpenMp-CS21BTECH11043.cpp -fopenmp
+  execution : ./openmp.out
+    	
+	
+	
+	
+
+
+
+
+
+
+	
+
+
+
+
+
+
---
+++ progassign-2/bash_const_K.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+g++-12 -o pthread.out Assgn2Srcpthread-CS21BTECH11043.cpp -lpthread
+g++-12 -o openmp.out Assgn2SrcOpenMp-CS21BTECH11043.cpp -fopenmp
+python3 sudoko.py 16 3
+for j in {3..10}
+do
+gawk  -v k=$j  -i inplace   'FNR==1{$2=k*k}1' input.txt
+for i in {1..10} 
+    do 
+      ./pthread.out
+      sleep 1
+    done >bash_output_pthread_$j.txt
+awk '{sum+=$1} END {print sum/NR}' bash_output_pthread_$j.txt >>bash_output_final_pthread.txt
+for i in {1..10} 
+    do 
+      ./openmp.out
+      sleep 1
+    done >bash_output_openmp_$j.txt
+awk '{sum+=$1} END {print sum/NR}' bash_output_openmp_$j.txt >>bash_output_final_openmp.txt
+done
+python3 plot_K_const.py plot_K_const.jpg 
+rm *bash_output*
+echo completed
+
